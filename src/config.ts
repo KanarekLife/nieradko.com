42c4225e086b2c4f75361e43e2bc3bfc4b0dff34
Stanisław Nieradko <stanislaw@nieradko.com>
2024-11-09 12:59:24 +0100

    add list view of articles and current page distinction in header

--- src/config.ts
+++ src/config.ts
@@ -11,5 +11,10 @@ export default {
         { type: 'LinkedIn', url: 'https://www.linkedin.com/in/stanislaw-nieradko/', name: 'Stanisław Nieradko', icon: LinkedInIcon },
         { type: 'Website', url: 'https://nieradko.com/', name: 'nieradko.com', icon: WebsiteIcon },
         { type: 'Email', url: 'mailto:stanislaw@nieradko.com', name: 'stanislaw@nieradko.com', icon: EmailIcon }
+    ],
+    menu: [
+        { name: 'Home', url: '/' },
+        { name: 'Articles', url: '/articles/' },
+        { name: 'Contact', url: '/contact/' }
     ]
 }
