d379caa37152cd01391b3d6e943f2e111127ca90
Stanisław Nieradko <stanislaw@nieradko.com>
2024-11-09 13:17:47 +0100

    add reading time

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
+import readingTime from './src/plugins/remark-reading-time.mjs';
 
 export default defineConfig({
-  integrations: [mdx(), tailwind({applyBaseStyles: false})]
+  integrations: [mdx(), tailwind({applyBaseStyles: false})],
+  markdown: {
+    remarkPlugins: [readingTime]
+  }
 });
