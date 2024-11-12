29c498a2ddeb43113727314ff9e8501636418cc6
Stanisław Nieradko <stanislaw@nieradko.com>
2024-11-12 16:44:35 +0100

    Finish and publish welcome-to-my-new-blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,11 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import readingTime from './src/plugins/remark-reading-time.mjs';
+import remarkEmoji from 'remark-emoji';
 
 export default defineConfig({
   integrations: [mdx(), tailwind({applyBaseStyles: false})],
   markdown: {
-    remarkPlugins: [readingTime]
+    remarkPlugins: [readingTime, remarkEmoji]
   }
 });
